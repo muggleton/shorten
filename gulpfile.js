739902debb09bceecccd2b7ce67a43dd6d04300f
David Muggleton <david@vibesfamily.com>
2015-07-06 16:15:04 +0100

    Add front end dependacies

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+
+var paths = {
+	'jquery': './resources/assets/bower/jquery/',
+	'bootstrap': './resources/assets/bower/bootstrap-sass-official/assets/'
+}
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+	mix.sass('app.scss', 'public/assets/css/', {includePaths: [paths.bootstrap + 'stylesheets/']})
+	.copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/assets/fonts')
+	.scripts([
+		paths.jquery + "dist/jquery.js",
+		paths.bootstrap + "javascripts/bootstrap.js"
+		], './', 'public/assets/js/app.js');
 });
